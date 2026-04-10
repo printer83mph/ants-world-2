3aa4a18a37078c1839c9250a3ed09fbed20ae738
Thomas Shaw <printer.83mph@gmail.com>
2026-04-10 06:07:32 -0400

    fix: typechecking for repo

--- backend/shared/repository/repo/abstract/users.py
+++ backend/shared/repository/repo/abstract/users.py
@@ -1,6 +1,5 @@
 import abc
 import uuid
-from typing import Optional
 
 from repo.models.users import User, UserCreate, UserUpdate
 
@@ -10,10 +9,10 @@ class UsersRepo(abc.ABC):
     async def create(self, create: UserCreate) -> User: ...
 
     @abc.abstractmethod
-    async def get_by_id(self, id: uuid.UUID) -> Optional[User]: ...
+    async def get_by_id(self, id: uuid.UUID) -> User | None: ...
 
     @abc.abstractmethod
-    async def get_by_username(self, username: str) -> Optional[User]: ...
+    async def get_by_username(self, username: str) -> User | None: ...
 
     @abc.abstractmethod
     async def update(self, id: uuid.UUID, update: UserUpdate) -> User: ...
