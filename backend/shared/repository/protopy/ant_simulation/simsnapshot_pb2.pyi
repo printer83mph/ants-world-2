5dcfcfaf986525ab8346d397096227e98d624fba
Thomas Shaw <printer.83mph@gmail.com>
2026-04-28 20:32:43 -0400

    Add protobuf model for simsnapshot

---
+++ backend/shared/repository/protopy/ant_simulation/simsnapshot_pb2.pyi
@@ -0,0 +1,51 @@
+from google.protobuf.internal import containers as _containers
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from collections.abc import Iterable as _Iterable, Mapping as _Mapping
+from typing import ClassVar as _ClassVar, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class SimSnapshot(_message.Message):
+    __slots__ = ("id", "ant_ids", "ants", "crumbs", "created_at", "updated_at")
+    ID_FIELD_NUMBER: _ClassVar[int]
+    ANT_IDS_FIELD_NUMBER: _ClassVar[int]
+    ANTS_FIELD_NUMBER: _ClassVar[int]
+    CRUMBS_FIELD_NUMBER: _ClassVar[int]
+    CREATED_AT_FIELD_NUMBER: _ClassVar[int]
+    UPDATED_AT_FIELD_NUMBER: _ClassVar[int]
+    id: str
+    ant_ids: _containers.RepeatedScalarFieldContainer[bytes]
+    ants: _containers.RepeatedCompositeFieldContainer[AntState]
+    crumbs: _containers.RepeatedCompositeFieldContainer[CrumbState]
+    created_at: int
+    updated_at: int
+    def __init__(self, id: _Optional[str] = ..., ant_ids: _Optional[_Iterable[bytes]] = ..., ants: _Optional[_Iterable[_Union[AntState, _Mapping]]] = ..., crumbs: _Optional[_Iterable[_Union[CrumbState, _Mapping]]] = ..., created_at: _Optional[int] = ..., updated_at: _Optional[int] = ...) -> None: ...
+
+class AntState(_message.Message):
+    __slots__ = ("x", "y", "rotation", "pheremone_sensitivity", "pheremone_strength", "speed", "seconds_of_life_left")
+    X_FIELD_NUMBER: _ClassVar[int]
+    Y_FIELD_NUMBER: _ClassVar[int]
+    ROTATION_FIELD_NUMBER: _ClassVar[int]
+    PHEREMONE_SENSITIVITY_FIELD_NUMBER: _ClassVar[int]
+    PHEREMONE_STRENGTH_FIELD_NUMBER: _ClassVar[int]
+    SPEED_FIELD_NUMBER: _ClassVar[int]
+    SECONDS_OF_LIFE_LEFT_FIELD_NUMBER: _ClassVar[int]
+    x: float
+    y: float
+    rotation: float
+    pheremone_sensitivity: float
+    pheremone_strength: float
+    speed: float
+    seconds_of_life_left: float
+    def __init__(self, x: _Optional[float] = ..., y: _Optional[float] = ..., rotation: _Optional[float] = ..., pheremone_sensitivity: _Optional[float] = ..., pheremone_strength: _Optional[float] = ..., speed: _Optional[float] = ..., seconds_of_life_left: _Optional[float] = ...) -> None: ...
+
+class CrumbState(_message.Message):
+    __slots__ = ("x", "y", "size")
+    X_FIELD_NUMBER: _ClassVar[int]
+    Y_FIELD_NUMBER: _ClassVar[int]
+    SIZE_FIELD_NUMBER: _ClassVar[int]
+    x: float
+    y: float
+    size: float
+    def __init__(self, x: _Optional[float] = ..., y: _Optional[float] = ..., size: _Optional[float] = ...) -> None: ...
